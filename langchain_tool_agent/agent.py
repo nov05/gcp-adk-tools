22305a76fec5bbbaa511eb0bd62ff5f549e14383
nov05 <1215590+nov05@users.noreply.github.com>
2026-04-07 18:44:27 -0500

    Add comment for LangChain Wikipedia tool addition
    Added a comment indicating the addition of the LangChain Wikipedia tool.

--- langchain_tool_agent/agent.py
+++ langchain_tool_agent/agent.py
@@ -30,6 +30,7 @@ root_agent = Agent(
     instruction="""Research the topic suggested by the user.
     Share the information you have found with the user.""",
     # Add the LangChain Wikipedia tool below
+    ## Added by Nov05 on 2026-04-07
     tools = [
         # Use the LangchainTool wrapper...
         LangchainTool(
